70009f60622398a8a082b67c508e6872f3f7ea17
reilley <reilleyluedde@nau.edu>
2020-06-03 10:00:09 -0700

    Implement gradient descent for lin. reg.
    There is now gradient descent and differentiation as the options
    for optimization algorithms. For this linear regression, it
    seems like differentiation is much cheaper, faster, and more
    accurate. I imagine gradient descent becomes pretty useful when
    you start using multi-dimensional algorithms.

--- run_linear_regression.py
+++ run_linear_regression.py
@@ -1,5 +1,13 @@
-from cenpy.products import Decennial2010
-
+import pandas as pd
 from linear_regression import LinearModel
+import statsmodels.api as sm
+
+df = pd.read_csv("homeprices.csv")
+lm = LinearModel(df, "area", "price","diff")
+print(lm.fit_model())
+
+sm_model = sm.OLS.from_formula("price ~ area", data = df)
+res = sm_model.fit()
+print(res.summary())
 
 
